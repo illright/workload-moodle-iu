511ecacfcf33ce18a1affb39111278c1a3b78e39
Lev Chelyadinov <leva181777@gmail.com>
2021-11-30 16:49:03 +0300

    Add a way to execute arbitrary JS within Moodle

--- block_workload.php
+++ block_workload.php
@@ -1,17 +1,27 @@
 <?php
 
+/** Credit: https://stackoverflow.com/a/53121360/5726823 */
+function path_join(...$parts) {
+    return preg_replace('#' . DIRECTORY_SEPARATOR . '+#', DIRECTORY_SEPARATOR, implode(DIRECTORY_SEPARATOR, array_filter($parts)));
+}
+
 class block_workload extends block_base {
     public function init() {
         $this->title = get_string('workload', 'block_workload');
     }
 
     public function get_content() {
-        if ($this->content !== null) {
-          return $this->content;
+        // if ($this->content !== null) {
+        //   return $this->content;
+        // }
+        if (strpos(getcwd(), 'my') !== false) {
+            $script = file_get_contents(path_join(__DIR__, 'index-alt.js'));
+        } else {
+            $script = file_get_contents(path_join(__DIR__, 'index.js'));
         }
 
-        $this->content         =  new stdClass;
-        $this->content->text   = 'Hello, World!';
+        $this->content = new stdClass;
+        $this->content->text = html_writer::empty_tag('div', array('id' => 'deployment_target')) . html_writer::tag('script', $script);
 
         return $this->content;
     }
---
+++ index.js
@@ -0,0 +1,1 @@
+document.querySelector('#deployment_target').innerHTML = '';
