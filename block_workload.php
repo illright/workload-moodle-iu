43d5eef96740a720cc49fff5e2777c9d3763491e
Lev Chelyadinov <leva181777@gmail.com>
2022-02-02 21:15:19 +0300

    Move DB queries to a separate file and implement script embedding

--- block_workload.php
+++ block_workload.php
@@ -8,102 +8,58 @@ function path_join(...$parts) {
 // For objects $DB, $USER, $COURSE
 require_once(path_join(__DIR__, '../../config.php'));
 
+require(path_join(__DIR__, './db_queries.php'));
 
-function get_upcoming_deadlines_from_course($course_id) {
-    global $DB;
 
-    return $DB->get_records_sql(
-        <<<EOS
-            SELECT
-                {assign}.id,
-                {assign}.name,
-                {assign}.duedate
-            FROM
-                {assign}
-            WHERE
-                {assign}.course = :course_id
-                AND {assign}.duedate > unix_timestamp(now());
-        EOS,
-        ['course_id' => $course_id]
-    );
-}
-
-function get_upcoming_deadlines_for_student($student_id) {
-    global $DB;
-
-    return $DB->get_records_sql(
-        <<<EOS
-            SELECT
-                {assign}.id,
-                {assign}.name,
-                {assign}.duedate
-                {course}.id AS course_id
-                {course}.fullname AS course_name
-            FROM
-                {assign}
-                INNER JOIN {course}
-                    ON {assign}.course = {course}.id
-            WHERE
-                {assign}.course IN (
-                    SELECT
-                        {enrol}.courseid
-                    FROM
-                        {enrol}
-                        INNER JOIN {user_enrolments}
-                            ON {enrol}.id = {user_enrolments}.enrolid
-                    WHERE
-                        {user_enrolments}.userid = :student_id
-                )
-                AND {assign}.duedate > unix_timestamp(now());
-        EOS,
-        ['student_id' => $student_id]
-    );
-}
-
-function get_ungraded_submissions_from_course($course_id, $student_id) {
-    global $DB;
-
-    return $DB->get_records_sql(
-        <<<EOS
-            SELECT
-                {assign}.id,
-                {assign}.name,
-                {assign}.gradingduedate,
-                SUM({assign_grades}.grade IS NOT NULL AND {assign_grades}.grade >= 0) AS submissions_graded,
-                COUNT(*) AS submissions_total
-            FROM
-                {assign_submission}
-                INNER JOIN {assign}
-                    ON {assign_submission}.assignment = {assign}.id
-                LEFT OUTER JOIN {assign_grades}
-                    ON {assign_submission}.assignment = {assign_grades}.assignment
-                    AND {assign_submission}.userid = {assign_grades}.userid
-                    AND {assign_submission}.attemptnumber = {assign_grades}.attemptnumber
-            WHERE
-                {assign}.course = :course_id
-                AND {assign_submission}.status = 'submitted'
-            GROUP BY
-                {assign}.id,
-                {assign}.name
-            HAVING
-                SUM(
-                    {assign_submission}.userid = :student_id
-                    AND ({assign_grades}.grade IS NULL OR {assign_grades}.grade < 0)
-                ) > 0
-        EOS,
-        ['course_id' => $course_id, 'student_id' => $student_id]
-    );
-}
-
-function is_student_dashboard_page() {
+/** Determines if the Moodle user is currently viewing their dashboard. */
+function is_on_dashboard_page() {
     return strpos(getcwd(), 'my') !== false;
 }
 
-function embed_js($script_path, $target_div_id = 'workload_target') {
+/** Determines if the Moodle user is currently viewing a course page. */
+function is_on_course_page() {
+    return strpos(getcwd(), 'course') !== false;
+}
+
+/**
+ * Determines if the Moodle user is a student.
+ *
+ * TODO: not implemented
+ */
+function is_student() {
+    return true;
+}
+
+function resolve_hashed_script($script_name, $script_directory = '.') {
+    $candidates = scandir($script_directory);
+    foreach ($candidates as $candidate) {
+        if (stripos($candidate, $script_name) == 0) {
+            return $candidate;
+        }
+    }
+
+    return NULL;
+}
+
+/** Generate a JS variable declaration with a given name and a JSON-serializable value. */
+function generate_declaration($variable_name, $value) {
+    return "const $variable_name = {json_encode($value)}";
+}
+
+/**
+ * Return a content object containing HTML with a target element and an inlined script.
+ *
+ * Optionally sets global variables for the script.
+ */
+function embed_js($script_path, $variables = [], $target_div_id = 'workload-target') {
     $script = file_get_contents(path_join(__DIR__, $script_path));
+
+    $init_data = array_map('generate_declaration', $variables).implode(';');
+
     $content = new stdClass;
     $content->text =
         html_writer::empty_tag('div', array('id' => $target_div_id))
+        . html_writer::tag('script', $init_data)
         . html_writer::tag('script', $script);
     return $content;
 }
@@ -116,29 +72,39 @@ class block_workload extends block_base {
     public function get_content() {
         global $USER;
         global $COURSE;
+        global $DB;
 
         // Commented out in hopes of faster reloading in development
         // if ($this->content !== null) {
         //   return $this->content;
         // }
 
-        $this->content = new stdClass;
-        if ($COURSE->id !== 1) {
-            $db_data = json_encode(
-                get_upcoming_deadlines_from_course($COURSE->id),
-                JSON_PRETTY_PRINT
-            ) . PHP_EOL . PHP_EOL . json_encode(
-                get_ungraded_submissions_from_course($COURSE->id, $USER->id),
-                JSON_PRETTY_PRINT
-            );
-        } else {
-            $db_data = json_encode(
-                get_upcoming_deadlines_for_student($USER->id),
-                JSON_PRETTY_PRINT
-            );
+        if (is_on_dashboard_page()) {
+            if (is_student()) {
+                $variables = array(
+                    'upcomingDeadlines' => get_upcoming_deadlines_for_student($DB, $USER->id),
+                );
+                $script_name = 'student-dashboard';
+            } else {
+                $script_name = NULL;
+            }
+        } else if (is_on_course_page()) {
+            if (is_student()) {
+                $variables = array(
+                    'upcomingDeadlines' => get_upcoming_deadlines_from_course($DB, $COURSE->id),
+                    'ungradedSubmissions' => get_ungraded_submissions_from_course($DB, $COURSE->id, $USER->id),
+                );
+                $script_name = 'student-course';
+            } else {
+                $variables = [];
+                $script_name = 'instructor-course';
+            }
         }
 
-        $this->content->text = html_writer::tag('pre', $db_data);
+        $this->content = embed_js(
+            resolve_hashed_script($script_name, path_join(__DIR__, './ui/dist/assets')),
+            $variables
+        );
 
         return $this->content;
     }
---
+++ db_queries.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Fetches all assignments of a given course whose due date is in the future.
+ *
+ * Required in the course view for both students and instructors.
+ */
+function get_upcoming_deadlines_from_course($DB, $course_id) {
+    return array_values($DB->get_records_sql(
+        <<<EOS
+            SELECT
+                {assign}.id,
+                {assign}.name,
+                {assign}.duedate
+            FROM
+                {assign}
+            WHERE
+                {assign}.course = :course_id
+                AND {assign}.duedate > unix_timestamp(now());
+        EOS,
+        ['course_id' => $course_id]
+    ));
+}
+
+/**
+ * Fetches all assignments of a given student whose due date is in the future.
+ *
+ * Required in the dashboard view for students.
+ */
+function get_upcoming_deadlines_for_student($DB, $student_id) {
+    return array_values($DB->get_records_sql(
+        <<<EOS
+            SELECT
+                {assign}.id,
+                {assign}.name,
+                {assign}.duedate
+                {course}.id AS course_id
+                {course}.fullname AS course_name
+            FROM
+                {assign}
+                INNER JOIN {course}
+                    ON {assign}.course = {course}.id
+            WHERE
+                {assign}.course IN (
+                    SELECT
+                        {enrol}.courseid
+                    FROM
+                        {enrol}
+                        INNER JOIN {user_enrolments}
+                            ON {enrol}.id = {user_enrolments}.enrolid
+                    WHERE
+                        {user_enrolments}.userid = :student_id
+                )
+                AND {assign}.duedate > unix_timestamp(now());
+        EOS,
+        ['student_id' => $student_id]
+    ));
+}
+
+/**
+ * Fetches all assignments of a given course that have ungraded submissions from a given student.
+ *
+ * Required in the course view for both students and instructors.
+ */
+function get_ungraded_submissions_from_course($DB, $course_id, $student_id) {
+    return array_values($DB->get_records_sql(
+        <<<EOS
+            SELECT
+                {assign}.id,
+                {assign}.name,
+                {assign}.gradingduedate,
+                SUM({assign_grades}.grade IS NOT NULL AND {assign_grades}.grade >= 0) AS submissions_graded,
+                COUNT(*) AS submissions_total
+            FROM
+                {assign_submission}
+                INNER JOIN {assign}
+                    ON {assign_submission}.assignment = {assign}.id
+                LEFT OUTER JOIN {assign_grades}
+                    ON {assign_submission}.assignment = {assign_grades}.assignment
+                    AND {assign_submission}.userid = {assign_grades}.userid
+                    AND {assign_submission}.attemptnumber = {assign_grades}.attemptnumber
+            WHERE
+                {assign}.course = :course_id
+                AND {assign_submission}.status = 'submitted'
+            GROUP BY
+                {assign}.id,
+                {assign}.name
+            HAVING
+                SUM(
+                    {assign_submission}.userid = :student_id
+                    AND ({assign_grades}.grade IS NULL OR {assign_grades}.grade < 0)
+                ) > 0
+        EOS,
+        ['course_id' => $course_id, 'student_id' => $student_id]
+    ));
+}
+
+?>
