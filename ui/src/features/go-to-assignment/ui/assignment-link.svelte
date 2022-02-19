7cf8abf9a063e03253a91bdaeaed632393ca4113
Lev Chelyadinov <leva181777@gmail.com>
2022-02-19 20:17:15 +0300

    Port the features layer to Svelte

---
+++ ui/src/features/go-to-assignment/ui/assignment-link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let id: number;
+
+  const moodleAssignmentURL = '/mod/assign/view.php';
+</script>
+
+<a
+  class="underline text-orange-500 leading-5"
+  href="{moodleAssignmentURL}?id={id}"
+>
+  <slot />
+</a>
