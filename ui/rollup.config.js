94ed67fb30e3a55e7d3b39fef069781f93bdc65c
Lev Chelyadinov <leva181777@gmail.com>
2022-02-19 19:07:12 +0300

    Configure several dev pages for each micro-frontend

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
@@ -30,54 +31,46 @@ function serve() {
 	};
 }
 
-export default {
-	input: 'src/main.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
-			}
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+const plugins = [
+	svelte({
+		preprocess: sveltePreprocess({ sourceMap: !production }),
+		compilerOptions: {
+			dev: !production
+		}
+	}),
+	css({ output: 'bundle.css' }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		typescript({
-			sourceMap: !production,
-			inlineSources: !production
-		}),
+	resolve({
+		browser: true,
+		dedupe: ['svelte']
+	}),
+	commonjs(),
+	typescript({
+		sourceMap: !production,
+		inlineSources: !production
+	}),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+	!production && serve(),
+	!production && livereload('public'),
+	production && terser()
+];
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
-};
+const INPUT_DIR = 'src/';
+export default fs
+	.readdirSync(INPUT_DIR)
+	.filter(file => file.endsWith('.entry.ts'))
+	.map(file => ({
+		input: INPUT_DIR + file,
+		output: {
+			inlineDynamicImports: true,
+			entryFileNames: `${file}.bundle.js`,
+			dir: 'public/build',
+			format: 'iife',
+			name: file,
+			sourcemap: false,
+		},
+		plugins,
+		watch: {
+			clearScreen: false
+		}
+	}));
