eb0f0fb2d560be9036c03d8e7df6c2622767f8a0
Lev Chelyadinov <leva181777@gmail.com>
2022-02-19 20:15:20 +0300

    Port the entities layer to Svelte

---
+++ ui/src/entities/assignment/ui/assignment-card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import GradingProgress, { type GradingProgressProps } from './grading-progress.svelte';
+  import { HumanReadableDate } from '@/shared/ui';
+
+  export let courseName: string | undefined;
+  export let submissionDeadline: Date | undefined;
+  export let gradingDeadline: Date | undefined;
+  export let gradingProgress: GradingProgressProps | undefined;
+</script>
+
+<div class="bg-white border border-gray-300 p-3 mb-2 last:mb-0">
+  <div class="flex flex-col">
+    {#if courseName !== undefined}
+      <p class="text-xs mb-1">{ courseName }</p>
+    {/if}
+    <slot name="assignment-name"></slot>
+  </div>
+  {#if submissionDeadline !== undefined}
+    <div class="mt-3 text-sm">
+      Due on <HumanReadableDate value={submissionDeadline} />
+    </div>
+  {/if}
+  {#if gradingDeadline !== undefined}
+    <div class="mt-3 text-sm">
+      To be graded by <HumanReadableDate value={gradingDeadline} />
+    </div>
+  {/if}
+  {#if gradingProgress !== undefined}
+    <GradingProgress {...gradingProgress} />
+  {/if}
+</div>
---
+++ ui/src/entities/assignment/ui/grading-progress.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+  export interface GradingProgressProps {
+    totalStudents: number;
+    submissions: number;
+    gradedSubmissions: number;
+  }
+</script>
+
+<script lang="ts">
+  import { s } from '@/shared/language';
+
+  export let totalStudents: number;
+  export let submissions: number;
+  export let gradedSubmissions: number;
+</script>
+
+<figure>
+  <div class="rounded border border-gray-300 bg-gray-50 h-2 mt-4 overflow-hidden">
+    <div class="bg-blue-300 h-full" style:width={`${submissions / totalStudents * 100}%`}>
+      <div class="bg-blue-600 h-full" style:width={`${gradedSubmissions / submissions * 100}%`} />
+    </div>
+  </div>
+  <figcaption class="text-sm">
+    <p class="mt-2">{submissions}/{totalStudents} student{s(submissions)} submitted</p>
+    <p class="mt-1">{gradedSubmissions}/{submissions} submission{s(gradedSubmissions)} graded</p>
+  </figcaption>
+</figure>
