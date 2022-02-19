a757a9ee31d3dfca956546b78703aa4e5edf4251
Lev Chelyadinov <leva181777@gmail.com>
2022-02-19 20:10:57 +0300

    Port the shared layer to Svelte

---
+++ ui/src/shared/ui/human-readable-date.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  const hourMs = 60 * 60 * 1000;
+  const dayMs = 24 * hourMs;
+
+  function estimate(date: Date) {
+    const differenceMs = date.valueOf() - Date.now();
+
+    if (differenceMs <= 0) {
+      return {
+        text: 'overdue',
+        critical: true,
+      };
+    }
+
+    if (differenceMs <= hourMs) {
+      return {
+        text: 'in an hour',
+        critical: true,
+      };
+    }
+
+    const hoursLeft = Math.ceil(differenceMs / hourMs);
+    if (differenceMs <= dayMs && hoursLeft <= 6) {
+      return {
+        text: `in ${hoursLeft} hours`,
+        critical: false,
+      };
+    }
+
+    const dayOfDate = Math.floor(date.valueOf() / dayMs);
+    const today = Math.floor(Date.now() / dayMs);
+
+    if (dayOfDate === today) {
+      return {
+        text: 'today',
+        critical: false,
+      };
+    }
+
+    if (dayOfDate === today + 1) {
+      return {
+        text: 'tomorrow',
+        critical: false,
+      };
+    }
+
+    const daysLeft = dayOfDate - today;
+    if (daysLeft < 30) {
+      return {
+        text: `in ${daysLeft} days`,
+        critical: false,
+      };
+    } else {
+      return {
+        text: 'not any time soon',
+        critical: false,
+      }
+    }
+  }
+
+  export let value: Date;
+  $: dateEstimate = estimate(value);
+</script>
+
+<strong>{value.toLocaleDateString()}</strong>
+&mdash;
+<span class={ dateEstimate.critical ? 'font-semibold text-red-700' : '' }>
+  { dateEstimate.text }
+</span>
---
+++ ui/src/shared/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as HumanReadableDate } from './human-readable-date.svelte';
