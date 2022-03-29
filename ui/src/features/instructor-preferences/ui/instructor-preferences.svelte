f313a3164f9967f91f8c49f78edbf3ab1918f4b0
Lev Chelyadinov <leva181777@gmail.com>
2022-03-29 22:43:40 +0500

    Add descriptive labels to options

--- ui/src/features/instructor-preferences/ui/instructor-preferences.svelte
+++ ui/src/features/instructor-preferences/ui/instructor-preferences.svelte
@@ -5,26 +5,33 @@
 <div class="title">Configure the workload calendar</div>
 <form>
   <fieldset>
-    <label>
+    <div class="field">
       <input
         type="checkbox"
         name="highlight"
+        id="workload-highlight-checkbox"
         value="true"
         bind:checked={$preferences.highlightThisCourse}
       />
-      Highlight days that have assignments from this course
-    </label>
+      <div>
+        <label for="workload-highlight-checkbox">
+          Highlight days that have assignments from this course
+        </label>
+        <div class="describing-text">Will give these days a blue border in the calendar</div>
+      </div>
+    </div>
   </fieldset>
   <fieldset>
     <p>Span of assignments:</p>
-    <label>
-      <input type="radio" name="span" value="span" bind:group={$preferences.binningMode} />
-      Every day from release to due date
-    </label>
-    <label>
-      <input type="radio" name="span" value="end" bind:group={$preferences.binningMode} />
-      Only the day the assignment is due
-    </label>
+    <div class="describing-text">Every day in the calendar has a counter. This determines which counters will an assignment increase</div>
+    <div class="field">
+      <input type="radio" name="span" id="workload-span-span" value="span" bind:group={$preferences.binningMode} />
+      <label for="workload-span-span">Every day from release to due date</label>
+    </div>
+    <div class="field">
+      <input type="radio" name="span" id="workload-span-end" value="end" bind:group={$preferences.binningMode} />
+      <label for="workload-span-end">Only the day the assignment is due</label>
+    </div>
   </fieldset>
 </form>
 
@@ -43,7 +50,11 @@
     margin-bottom: 0.5rem;
   }
 
-  label {
+  fieldset p {
+    margin-bottom: 0;
+  }
+
+  .field {
     display: flex;
     gap: 0.5rem;
     margin: 0.25rem 0;
@@ -55,5 +66,10 @@
 
   input {
     margin-top: 0.25rem;
+  }
+
+  .describing-text {
+    font-size: 0.9em;
+    font-style: italic;
   }
 </style>
